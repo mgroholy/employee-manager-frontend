0e5335ca17cbb97a110011eb719ca539779dbd6a
Kriszti Toth <k95toth@gmail.com>
2021-04-07 11:39:27 +0200

    feat: use MUI accordion for displaying employee details

--- src/components/EmployeeDetail.js
+++ src/components/EmployeeDetail.js
@@ -1,12 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import Typography from "@material-ui/core/Typography";
 
 const EMPLOYEE_REST_API_URL = "http://localhost:8080/employees/";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: "33.33%",
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+}));
+
 const EmployeeDetail = () => {
+  const classes = useStyles();
   const { id } = useParams();
-  const [employee, setEmployee] = useState([]);
+  const [employee, setEmployee] = useState({});
+
+  let employeeAttributes = Object.keys(employee);
+  const index = employeeAttributes.indexOf("Name");
+  if (index > -1) {
+    employeeAttributes.splice(index, 1);
+  }
 
   useEffect(() => {
     const fetchEmployee = async () => {
@@ -17,7 +43,23 @@ const EmployeeDetail = () => {
     fetchEmployee();
   }, [id]);
 
-  return <div>{employee.name}</div>;
+  return (
+    <div className={classes.root}>
+      <h1>{employee.Name}</h1>
+      <div>
+        {employeeAttributes.map((attribute, index) => (
+          <Accordion key={index}>
+            <AccordionSummary>
+              <Typography className={classes.heading}>{attribute}</Typography>
+              <Typography className={classes.secondaryHeading}>
+                {employee[attribute]}
+              </Typography>
+            </AccordionSummary>
+          </Accordion>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default EmployeeDetail;
