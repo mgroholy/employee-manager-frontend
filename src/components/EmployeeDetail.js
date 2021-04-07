9617970e216c0e5db7fb025b18d01ca3ef1a1386
Kriszti Toth <k95toth@gmail.com>
2021-04-07 12:13:25 +0200

    feat: implement header with action buttons for detail page

--- src/components/EmployeeDetail.js
+++ src/components/EmployeeDetail.js
@@ -5,6 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
+import EmployeeDetailHeader from "./EmployeeDetailHeader";
 
 const EMPLOYEE_REST_API_URL = "http://localhost:8080/employees/";
 
@@ -45,7 +46,7 @@ const EmployeeDetail = () => {
 
   return (
     <div className={classes.root}>
-      <h1>{employee.Name}</h1>
+      <EmployeeDetailHeader employeeName={employee.Name} />
       <div>
         {employeeAttributes.map((attribute, index) => (
           <Accordion key={index}>
