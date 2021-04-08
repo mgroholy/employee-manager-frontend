47f054ad61787adbc003df23e71e1774f79ced41
Máté Groholy <mate.groholy@gmail.com>
2021-04-08 14:01:06 +0200

    feat: modify user input filter section design and spacing.

--- src/components/EmployeeFilter.js
+++ src/components/EmployeeFilter.js
@@ -11,6 +11,7 @@ import {
   FormControlLabel,
   FormLabel,
   Button,
+  Typography,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React, { useState } from "react";
@@ -19,7 +20,13 @@ const useStyles = makeStyles(() => ({
   formContainer: {
     display: "flex",
     alignItems: "center",
+    justifyContent: "space-between",
+  },
+  userInputContainer: {
+    display: "flex",
+    alignItems: "center",
     justifyContent: "space-around",
+    width: "55%",
   },
 }));
 
@@ -52,7 +59,12 @@ const EmployeeFilter = ({
   return (
     <Box marginBottom={3}>
       <Paper elevation={2}>
-        <Box padding={2} className={classes.formContainer}>
+        <Box
+          padding={2}
+          paddingLeft={5}
+          paddingRight={5}
+          className={classes.formContainer}
+        >
           <FormControl className={formControlClass} variant={"outlined"}>
             <InputLabel id="department-select-label">Department</InputLabel>
             <Select
@@ -79,62 +91,74 @@ const EmployeeFilter = ({
               ))}
             </Select>
           </FormControl>
-
-          <FormControl component="fieldset">
-            <FormLabel component="legend">Type</FormLabel>
-            <RadioGroup
-              row
-              aria-label="type"
-              name="type"
-              defaultValue="Name"
+          <Box className={classes.userInputContainer}>
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Type</FormLabel>
+              <RadioGroup
+                row
+                aria-label="type"
+                name="type"
+                defaultValue="Name"
+                onChange={(event) => {
+                  setRadioOption(event.target.value);
+                }}
+              >
+                <FormControlLabel
+                  value="Name"
+                  control={<Radio size="small" color="primary" />}
+                  label={
+                    <Typography variant="body2" color="textSecondary">
+                      Name
+                    </Typography>
+                  }
+                  selected={true}
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="Email"
+                  control={<Radio size="small" color="primary" />}
+                  label={
+                    <Typography variant="body2" color="textSecondary">
+                      Email
+                    </Typography>
+                  }
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="Id"
+                  control={<Radio size="small" color="primary" />}
+                  label={
+                    <Typography variant="body2" color="textSecondary">
+                      Id
+                    </Typography>
+                  }
+                  labelPlacement="bottom"
+                />
+              </RadioGroup>
+            </FormControl>
+            <TextField
+              id="outlined-basic"
+              label={radioOption}
+              variant="outlined"
+              value={userInput}
+              helperText={inputError}
+              error={inputError !== ""}
               onChange={(event) => {
-                setRadioOption(event.target.value);
+                setInputError("");
+                setUserInput(event.target.value);
+              }}
+            />
+            <Button
+              size="large"
+              variant="outlined"
+              color="secondary"
+              onClick={() => {
+                filterByUserInput();
               }}
             >
-              <FormControlLabel
-                value="Name"
-                control={<Radio color="primary" />}
-                label="Name"
-                selected={true}
-                labelPlacement="bottom"
-              />
-              <FormControlLabel
-                value="Email"
-                control={<Radio color="primary" />}
-                label="Email"
-                labelPlacement="bottom"
-              />
-              <FormControlLabel
-                value="Id"
-                control={<Radio color="primary" />}
-                label="Id"
-                labelPlacement="bottom"
-              />
-            </RadioGroup>
-          </FormControl>
-          <TextField
-            margin={"normal"}
-            id="outlined-basic"
-            label={radioOption}
-            variant="outlined"
-            value={userInput}
-            helperText={inputError}
-            error={inputError !== ""}
-            onChange={(event) => {
-              setInputError("");
-              setUserInput(event.target.value);
-            }}
-          />
-          <Button
-            variant="outlined"
-            margin={"normal"}
-            color="secondary"
-            onClick={() => {
-              filterByUserInput();
-            }}
-          >
-            Filter
-          </Button>
+              Filter
+            </Button>
+          </Box>
         </Box>
       </Paper>
     </Box>
