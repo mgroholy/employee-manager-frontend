8ac4c779bf51ccb371c0585efdc3310d0c1a5f16
Máté Groholy <mate.groholy@gmail.com>
2021-04-20 11:49:42 +0200

    feat: modifyAddDepartment component to be a smaller module inside DepartmentList.

--- src/components/AddDepartment.js
+++ src/components/AddDepartment.js
@@ -3,6 +3,7 @@ import {
   Button,
   Container,
   FormControl,
+  makeStyles,
   Paper,
   TextField,
   Typography,
@@ -10,59 +11,83 @@ import {
 import axios from "axios";
 import React, { useState } from "react";
 
-const AddDepartment = () => {
+const useStyles = makeStyles(() => ({
+  form: {
+    display: "flex",
+    flexDirection: "row",
+  },
+}));
+
+const AddDepartment = ({ fetchDepartments }) => {
+  const classes = useStyles();
+
   const DEPARTMENTS_REST_API_URL = "http://localhost:8080//departments";
 
   const [department, setDepartment] = useState({ name: "" });
   const [error, setError] = useState("");
+  const [activeForm, setActiveForm] = useState(false);
+
+  const toggleActiveForm = () => {
+    setActiveForm(!activeForm);
+  };
 
   const sendDepartment = async () => {
     try {
       const response = await axios.post(DEPARTMENTS_REST_API_URL, department);
       console.log(response);
+      fetchDepartments();
+      toggleActiveForm();
+      setDepartment({ name: "" });
     } catch (error) {
       setError(error.response.data.message);
     }
   };
 
   return (
-    <Container>
-      <Paper elevation={2}>
-        <Box p={5}>
-          <Typography variant="h3">Add department</Typography>
-          <FormControl fullWidth={true}>
-            <Box paddingTop={3}>
-              <TextField
-                margin={"normal"}
-                fullWidth={true}
-                id="outlined-basic"
-                label="Department name:"
-                variant="outlined"
-                required={true}
-                onChange={(event) => {
-                  setError("");
-                  setDepartment({ ...department, name: event.target.value });
-                }}
-                helperText={error}
-                error={error !== ""}
-              />
-              <Button
-                variant="outlined"
-                margin={"normal"}
-                color="secondary"
-                type="submit"
-                onClick={(event) => {
-                  event.preventDefault();
-                  sendDepartment();
-                }}
-              >
-                Submit
-              </Button>
-            </Box>
-          </FormControl>
-        </Box>
-      </Paper>
-    </Container>
+    <FormControl>
+      {activeForm ? (
+        <>
+          <TextField
+            margin={"normal"}
+            // fullWidth={true}
+            id="outlined-basic"
+            label="Department name:"
+            variant="outlined"
+            required={true}
+            value={department.name}
+            onChange={(event) => {
+              setError("");
+              setDepartment({ ...department, name: event.target.value });
+            }}
+            helperText={error}
+            error={error !== ""}
+          />
+          <Button
+            variant="outlined"
+            margin={"normal"}
+            color="secondary"
+            type="submit"
+            onClick={(event) => {
+              event.preventDefault();
+              sendDepartment();
+            }}
+          >
+            Submit
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button
+            variant="outlined"
+            margin={"normal"}
+            color="secondary"
+            onClick={() => toggleActiveForm()}
+          >
+            Add Department
+          </Button>
+        </>
+      )}
+    </FormControl>
   );
 };
 
