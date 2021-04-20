be4a537e317166543e53a30c03bce379f462b01a
Máté Groholy <mate.groholy@gmail.com>
2021-04-20 17:18:30 +0200

    refactor: modify AddEmployee import location.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ import EmployeeList from "./components/EmployeeList";
 import Toolbar from "@material-ui/core/Toolbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Fragment } from "react";
-import AddEmployee from "./components/AddEmployee";
+import AddEmployee from "./components/AddEmployee/AddEmployee";
 import EmployeeDetail from "./components/EmployeeDetail";
-import AddDepartment from "./components/AddDepartment";
 import DepartmentList from "./components/DepartmentList";
 
 const useStyles = makeStyles((theme) => ({
