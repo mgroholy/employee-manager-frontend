7dd4d545f6d2abf66751fdcf13007da02b4c7ed8
Máté Groholy <mate.groholy@gmail.com>
2021-04-20 11:26:21 +0200

    feat: create DepartmentList component.

---
+++ src/components/DepartmentList.js
@@ -0,0 +1,94 @@
+import {
+  Container,
+  Paper,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  makeStyles,
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const useStyles = makeStyles(() => ({
+  header: {
+    fontWeight: "bold",
+    background: "#333",
+    color: "#fff",
+  },
+  headerCell: {
+    color: "#fff",
+    fontSize: "1.2rem",
+    fontWeight: "bold",
+  },
+  formControl: {
+    minWidth: "150px",
+  },
+  tableCell: {},
+}));
+
+const DepartmentList = () => {
+  const DEPARTMENTS_REST_API_URL = "http://localhost:8080//departments";
+
+  const [departments, setDepartments] = useState([]);
+
+  const fetchDepartments = async () => {
+    const response = await axios.get(DEPARTMENTS_REST_API_URL);
+    console.log(response);
+    setDepartments(response.data);
+  };
+
+  const deleteDepartment = async (departmentId) => {
+    console.log("delete", departmentId);
+    await axios.delete(`${DEPARTMENTS_REST_API_URL}/${departmentId}/delete`);
+    fetchDepartments();
+  };
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    fetchDepartments();
+  }, []);
+
+  return (
+    <Container>
+      <TableContainer component={Paper} elevation={2}>
+        <Table>
+          <TableHead>
+            <TableRow className={classes.header}>
+              <TableCell className={classes.headerCell}>
+                Department Name
+              </TableCell>
+              <TableCell className={classes.headerCell}>
+                Number of Employees
+              </TableCell>
+              <TableCell className={classes.headerCell}>Delete</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {departments.map((department) => (
+              <TableRow key={department.id} className={classes.tableRow}>
+                <TableCell>{department.name}</TableCell>
+                <TableCell className={classes.tableCell}>
+                  {department.employeeCount}
+                </TableCell>
+                <TableCell className={classes.tableCell}>
+                  <DeleteIcon onClick={() => deleteDepartment(department.id)} />
+                </TableCell>
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell>Add Department</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
+};
+
+export default DepartmentList;
