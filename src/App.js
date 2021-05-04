926b15133daf1c0c77c3215351ec57bce23aed91
Máté Groholy <mate.groholy@gmail.com>
2021-05-04 11:27:05 +0200

    feat: move ROuter to index.js, to use useLocation in App.js.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TopBar from "./components/bars/TopBar";
 import SideBar from "./components/bars/SideBar";
 import EmployeeList from "./components/EmployeeList/EmployeeList";
 import Toolbar from "@material-ui/core/Toolbar";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, useLocation } from "react-router-dom";
 import AddEmployee from "./components/AddEmployee/AddEmployee";
 import EmployeeDetail from "./components/EmployeeDetails/EmployeeDetail";
 import DepartmentList from "./components/DepartmentList/DepartmentList";
@@ -23,27 +23,24 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
-  const pathname = window.location.pathname;
+  const location = useLocation();
 
   return (
-    <Router>
-      <div className={classes.root}>
-        <CssBaseline />
-        <TopBar />
-        {console.log(pathname)}
-        {pathname !== "/sign-in" && <SideBar />}
+    <div className={classes.root}>
+      <CssBaseline />
+      <TopBar />
+      {location.pathname !== "/sign-in" && <SideBar />}
 
-        <main className={classes.content}>
-          <Toolbar />
-          <Route exact path="/" component={EmployeeList} />
-          <Route exact path="/employees" component={EmployeeList} />
-          <Route exact path="/add-employee" component={AddEmployee} />
-          <Route exact path="/employees/:id" component={EmployeeDetail} />
-          <Route exact path="/departments" component={DepartmentList} />
-          <Route exact path="/sign-in" component={Login} />
-        </main>
-      </div>
-    </Router>
+      <main className={classes.content}>
+        <Toolbar />
+        <Route exact path="/" component={EmployeeList} />
+        <Route exact path="/employees" component={EmployeeList} />
+        <Route exact path="/add-employee" component={AddEmployee} />
+        <Route exact path="/employees/:id" component={EmployeeDetail} />
+        <Route exact path="/departments" component={DepartmentList} />
+        <Route exact path="/sign-in" component={Login} />
+      </main>
+    </div>
   );
 }
 
