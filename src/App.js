3c29504b2df15de40747b94e4d1f60ea790f3140
Kriszti Toth <k95toth@gmail.com>
2021-05-04 21:40:27 +0200

    feat: store user in global context

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import AddEmployee from "./components/AddEmployee/AddEmployee";
 import EmployeeDetail from "./components/EmployeeDetails/EmployeeDetail";
 import DepartmentList from "./components/DepartmentList/DepartmentList";
 import Login from "./components/Login/Login";
+import { UserContext } from "./components/UserContext";
+import CurrentUser from "./components/CurrentUser";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,22 +27,26 @@ function App() {
   const classes = useStyles();
   const location = useLocation();
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <TopBar />
-      {location.pathname !== "/sign-in" && <SideBar />}
+  const { user, setUser, isLoading } = CurrentUser();
 
-      <main className={classes.content}>
-        <Toolbar />
-        <Route exact path="/" component={EmployeeList} />
-        <Route exact path="/employees" component={EmployeeList} />
-        <Route exact path="/add-employee" component={AddEmployee} />
-        <Route exact path="/employees/:id" component={EmployeeDetail} />
-        <Route exact path="/departments" component={DepartmentList} />
-        <Route exact path="/sign-in" component={Login} />
-      </main>
-    </div>
+  return (
+    <UserContext.Provider value={{ user, setUser, isLoading }}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <TopBar />
+        {location.pathname !== "/sign-in" && <SideBar />}
+
+        <main className={classes.content}>
+          <Toolbar />
+          <Route exact path="/" component={EmployeeList} />
+          <Route exact path="/employees" component={EmployeeList} />
+          <Route exact path="/add-employee" component={AddEmployee} />
+          <Route exact path="/employees/:id" component={EmployeeDetail} />
+          <Route exact path="/departments" component={DepartmentList} />
+          <Route exact path="/sign-in" component={Login} />
+        </main>
+      </div>
+    </UserContext.Provider>
   );
 }
 
