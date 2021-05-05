170a2e757a456f5a2218ea08b98f0312d91ae78a
Kriszti Toth <k95toth@gmail.com>
2021-05-05 13:34:02 +0200

    feat: store user roles in context

--- src/App.js
+++ src/App.js
@@ -28,10 +28,10 @@ function App() {
   const classes = useStyles();
   const location = useLocation();
 
-  const { user, setUser, isLoading } = CurrentUser();
+  const { user, setUser, roles, isLoading } = CurrentUser();
 
   return (
-    <UserContext.Provider value={{ user, setUser, isLoading }}>
+    <UserContext.Provider value={{ user, setUser, roles, isLoading }}>
       <div className={classes.root}>
         <CssBaseline />
         <TopBar />
