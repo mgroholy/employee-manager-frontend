5b03a7f4d1119e13bf3a2ff1083e985ebe87e360
Máté Groholy <mate.groholy@gmail.com>
2021-05-05 14:37:07 +0200

    feat: rename FeedBackModal to ConfirmModal. Add support for two buttons and onclick handling.

---
+++ src/components/FeedbackModal/ConfirmModal.js
@@ -0,0 +1,65 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from "@material-ui/core";
+import React, { useState } from "react";
+
+const ConfirmModal = ({
+  open,
+  toggleOpen,
+  dialogContent,
+  setDialogContent,
+  dialogButtonOneText,
+  setChoice,
+  onClickAction,
+  dialogButtonTwoText,
+  setDialogButtonOneText,
+  setDialogButtonTwoText,
+}) => {
+  return (
+    <Dialog
+      open={open}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {dialogContent}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => {
+            toggleOpen();
+            setChoice && setChoice(true);
+            onClickAction && onClickAction();
+            setDialogContent && setDialogContent("");
+            setDialogButtonOneText && setDialogButtonOneText("");
+            setDialogButtonTwoText && setDialogButtonTwoText("");
+          }}
+          color="primary"
+        >
+          {dialogButtonOneText}
+        </Button>
+        {dialogButtonTwoText && (
+          <Button
+            onClick={() => {
+              toggleOpen();
+              setDialogContent && setDialogContent("");
+              setDialogButtonOneText && setDialogButtonOneText("");
+              setDialogButtonTwoText && setDialogButtonTwoText("");
+            }}
+            color="primary"
+          >
+            {dialogButtonTwoText}
+          </Button>
+        )}
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmModal;
